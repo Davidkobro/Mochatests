da455fbc7c3144f85dcd0685096aaca8da42f8c3
David Kob <Davidkob@MacBook-Pro-59.local>
2018-06-20 15:37:53 -0400

    final push for cleaning

--- test/appTest.js
+++ test/appTest.js
@@ -1,21 +1,36 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+//const sayHello = require('../app').sayHello;
+//const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+//Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 
 describe('App', function() {
+    describe('sayHello()', function(){
     it('app should return hello', function(){
-        let result = sayHello();
-        assert.equal(result, 'hello');
+        let result = app.sayHello();
+        assert.equal(sayHelloResult, 'hello');
     });
 
     it('sayHello should return type string', function(){ 
-        let result = sayHello();
-        assert.typeOf(result, 'string');
+        let result = app.sayHello();
+        assert.typeOf(sayHelloResult, 'string');
+    });
+});
 
-    })
+    describe('addNumbers()', function(){
+        it('addNumbers should be above 5', function() {
+            let result = app.addNumbers(5,5);
+            assert.isAbove(addNumbersResult, 5);
+        });
+       
+        it('addNumber should return type number', function(){ 
+            let result = app.addNumbers(5,5);
+            assert.typeOf(addNumbersResult, 'number');
+        });
+    
+    });
 
-    it('addNumbers should be above 5', function() {
-        let result = addNumbers(5,5);
-        assert.isAbove(result, 5);
-    })
 });
